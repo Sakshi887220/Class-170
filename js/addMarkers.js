942d2ea064352288e5c5a2ab1995875cd2b66005
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-12-25 10:40:48 +0530

    Update addMarkers.js

--- js/addMarkers.js
+++ js/addMarkers.js
@@ -8,29 +8,13 @@ AFRAME.registerComponent("create-markers", {
     var dishes = await this.getDishes();
    
     dishes.map(dish => {
-      var marker = document.createElement("a-marker");   
-      marker.setAttribute("id", dish.id);
-      marker.setAttribute("type", "pattern");
-      marker.setAttribute("url", dish.marker_pattern_url);
-      marker.setAttribute("cursor", {
-        rayOrigin: "mouse"
+     
       });
 
       //set the markerhandler component
-      marker.setAttribute("markerhandler", {});
-      mainScene.appendChild(marker);
-
-      // Adding 3D model to scene
-      var model = document.createElement("a-entity");    
      
-      model.setAttribute("id", `model-${dish.id}`);
-      model.setAttribute("position", dish.model_geometry.position);
-      model.setAttribute("rotation", dish.model_geometry.rotation);
-      model.setAttribute("scale", dish.model_geometry.scale);
-      model.setAttribute("gltf-model", `url(${dish.model_url})`);
-      model.setAttribute("gesture-handler", {});
-      marker.appendChild(model);
-
+      // Adding 3D model to scene
+      
       // Ingredients Container
       var mainPlane = document.createElement("a-plane");
       mainPlane.setAttribute("id", `main-plane-${dish.id}`);
@@ -51,19 +35,7 @@ AFRAME.registerComponent("create-markers", {
       mainPlane.appendChild(titlePlane);
 
       // Dish title
-      var dishTitle = document.createElement("a-entity");
-      dishTitle.setAttribute("id", `dish-title-${dish.id}`);
-      dishTitle.setAttribute("position", { x: 0, y: 0, z: 0.1 });
-      dishTitle.setAttribute("rotation", { x: 0, y: 0, z: 0 });
-      dishTitle.setAttribute("text", {
-        font: "monoid",
-        color: "black",
-        width: 1.8,
-        height: 1,
-        align: "center",
-        value: dish.dish_name.toUpperCase()
-      });
-      titlePlane.appendChild(dishTitle);
+      
 
       // Ingredients List
       var ingredients = document.createElement("a-entity");
@@ -82,12 +54,6 @@ AFRAME.registerComponent("create-markers", {
   },
   //function to get the dishes collection from firestore database
   getDishes: async function() {
-    return await firebase
-      .firestore()
-      .collection("dishes")
-      .get()
-      .then(snap => {
-        return snap.docs.map(doc => doc.data());
-      });
+   
   }
 });
